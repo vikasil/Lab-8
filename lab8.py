e51367e4887efe7ff5e131432ab2b1b35d5fe195
Viktoria Silanteva <vika.silanteva.05@list.ru>
2024-04-06 21:58:35 +0300

    Update lab8.py

--- lab8.py
+++ lab8.py
@@ -4,7 +4,7 @@ import cv2
 def task1():
     image = cv2.imread("images/variant-6.png")
     image_X2 = cv2.resize(image, None, fx=2, fy=2)
-    while (cv2.waitKey(1) != 27):#выход по кнопке Esc
+    while cv2.waitKey(1) != 27:#выход по кнопке Esc
         cv2.imshow("image", image) 
         cv2.imshow("image_X2", image_X2) 
     cv2.waitKey(0)
@@ -13,7 +13,7 @@ def task1():
 
 def task2():
     cap = cv2.VideoCapture("sample.mp4")
-    while (cv2.waitKey(1) != 27):
+    while cv2.waitKey(1) != 27:
         ret, image = cap.read()
         if not ret:
             break
@@ -24,7 +24,7 @@ def task2():
         if len(contours) > 0:
             c = max(contours, key=cv2.contourArea)
             x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.imshow("image", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -35,34 +35,34 @@ def task3():
     Left = 0
     Right = 0
     cap = cv2.VideoCapture("sample.mp4")
-    while (cv2.waitKey(1) != 27):
+    while cv2.waitKey(1) != 27:
         ret, image = cap.read()
         if not ret:
             break
-        mask=cv2.inRange(image, (80, 80, 80), (255, 255, 255))
+        mask=cv2.inRange(image,(80,80,80),(255,255,255))
         ret, thresh = cv2.threshold(mask, 110, 255, cv2.THRESH_BINARY_INV)
         contours, hierarchy = cv2.findContours(thresh,
-                                                cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                                               cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         if len(contours) > 0:
             c = max(contours, key=cv2.contourArea)
             x, y, w, h = cv2.boundingRect(c)
             cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
         centreX = image.shape[1]//2
-        if x > (centreX):
+        if x>(centreX):
             NEWposition = 2
-        if x + w < (centreX):
+        if x+w<(centreX):
             NEWposition = 1
         if NEWposition != OLDposition:
             if OLDposition == 2:
-                Left += 1
+                Left+=1
             if OLDposition == 1:
-                Right += 1
-            OLDposition = NEWposition
+                Right+=1
+            OLDposition=NEWposition
         cv2.putText(image,  str(Left), (100,100),
-                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0) , 2)
         cv2.putText(image,  str(Right), (500,100),
-                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        cv2.imshow("image", image)
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0) , 2)
+        cv2.imshow("image",image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -72,7 +72,7 @@ def additional_task():
     Left = 0
     Right = 0
     cap = cv2.VideoCapture("sample.mp4")
-    while (cv2.waitKey(1) != 27):
+    while cv2.waitKey(1) != 27:
         ret, image = cap.read()
         if not ret:
             break
